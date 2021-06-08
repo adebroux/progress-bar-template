bcf480b1858178ce606c117e30016a49ff3cb194
adebroux <adebroux@wustl.edu>
2021-06-08 09:15:07 -0500

    Have placeholder image, but initializes strangely

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -3,8 +3,21 @@ import { Text, View, StyleSheet } from 'react-native'
 
 const ProgressBar = props => {
     const { variable, min, max, color, secondaryColor } = props
-    const firstBar = variable - min
-    const secondBar = max - variable
+    console.log(typeof (variable))
+    console.log("Value:" + variable)
+    let firstBar = variable - min
+    let secondBar = max - variable
+    if (variable == 0) {
+        if (max == 0) {
+            if (min == 0) {
+                firstBar = 0.25
+                secondBar = 0.75
+            }
+            else {
+                secondBar = 1
+            }
+        }
+    }
     return (
         <View style={styles.wrapper}>
             <View style={{ backgroundColor: secondaryColor, flex: firstBar, height: 10 }} />
