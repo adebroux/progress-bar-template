7c9860de83d505f623a0f847bf3106ef4869cc14
adebroux <adebroux@wustl.edu>
2021-06-08 15:10:21 -0500

    Updated colors, height, ===, and skeleton props

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
 const ProgressBar = props => {
-    const { title, variable, min, max, color, secondaryColor } = props
+    const { variable, min, max, color, secondaryColor, titleSection } = props
+    const { title } = titleSection
+
+    useEffect(() => {
+
+    }, [variable])
+
     let firstBar = variable - min
     let secondBar = max - variable
-    if (variable == 0) {
-        if (max == 0) {
-            if (min == 0) {
+    if (variable === 0) {
+        if (max === 0) {
+            if (min === 0) {
                 firstBar = 0.25
                 secondBar = 0.75
             }
@@ -16,14 +22,24 @@ const ProgressBar = props => {
             }
         }
     }
+
+    if (typeof (min) !== 'number') {
+        min = 0
+    }
+
+    if (typeof (max) !== 'number') {
+        max = min + 10
+    }
+
+    if (typeof (variable) !== 'number') {
+        variable = (min + max) / 2
+    }
+
     return (
-        <View>
-            <Text>{title}</Text>
             <View style={styles.wrapper}>
                 <View style={{ backgroundColor: secondaryColor, flex: firstBar, height: 10 }} />
                 <View style={{ backgroundColor: color, flex: secondBar, height: 10 }} />
             </View>
-        </View>
     )
 }
 
@@ -32,7 +48,6 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
-    height: 50,
     padding: 20,
     flexDirection: "row",
   },
